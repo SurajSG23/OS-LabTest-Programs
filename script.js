34e94dde7bfef7b5abc4c57747606bae7f419623
Suraj <surajsgd23@gmail.com>
2024-09-04 07:51:52 +0530

    Programs

---
+++ script.js
@@ -0,0 +1,22 @@
+let copyF = (e) => {
+
+    let copyText = document.getElementsByClassName("accordion-body" + e)[0].innerText.replaceAll("&nbsp;", ' ');
+    navigator.clipboard.writeText(copyText);
+    document.getElementsByClassName("accordion-body" + e)[0].parentNode.childNodes[1].innerText = "Copied";
+    document.getElementsByClassName("accordion-body" + e)[0].parentNode.childNodes[1].style.backgroundColor = "rgb(163, 253, 114)"
+    setTimeout(() => {
+        document.getElementsByClassName("accordion-body" + e)[0].parentNode.childNodes[1].innerText = "Copy";
+        document.getElementsByClassName("accordion-body" + e)[0].parentNode.childNodes[1].style.backgroundColor = "#d6d6d6"
+    }, 2000);
+}
+
+async function Programs() {
+    for (let index = 1; index < 25; index++) {
+        let programs = await fetch("http://127.0.0.1:5500/Programs/Programs" + index + ".txt")
+        let response = await programs.text();
+        document.getElementsByClassName("accordion-body" + index)[0].innerText = response
+    }
+
+}
+
+Programs()
